2401080fdfb6449f8ba90051426b2d910ce2781b
tommyashpiyush <56206609+tommyashpiyush@users.noreply.github.com>
2019-12-08 22:32:40 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 module.exports = {
     menu: require('./menu'),
-    reactions: {first: '⏪', back: '◀', next: '▶', last: '⏩', stop: '⏹'}
+    reactions: {first: '1⃣', second: '2⃣', third: '3⃣', four: '4⃣', stop: '⏹'}
 }
